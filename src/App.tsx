278f3211ae58a10492d317b64675cb7557083270
Alexey <falconaleksey@mail.ru>
2022-08-03 15:48:14 +0400

    appbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import { Menu } from '@material-ui/icons';
+import {AppBar, Button, Container, IconButton, Toolbar, Typography} from '@mui/material';
 import React from 'react';
-import './App.css';
+import './App.module.css';
 import { Todolist } from './components/Todolist';
 
 function App() {
   return (
     <div className="App">
-       <Todolist title={'what to learn'} filter={'all'}/>
-       <Todolist title={'what to buy'} filter={'active'}/>
+        <div className="App">
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant="h6">
+                        Todo App
+                    </Typography>
+                    <Button color="inherit" >Login</Button>
+                </Toolbar>
+            </AppBar>
+            <Container fixed>
+                <Todolist title={'what to learn'} filter={'all'}/>
+                <Todolist title={'what to buy'} filter={'active'}/>
+            </Container>
+        </div>
+
     </div>
   );
 }
